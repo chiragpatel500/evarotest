132fe74c88e0e1073e54461d91a69778041fe394
chiragpatel500 <chirag12354@gmail.com>
2021-08-10 08:24:59 +0200

    npm update

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,18 @@
-import React from "react";
+
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import TextField from "@material-ui/core/TextField";
-// import AdapterDateFns from "@material-ui/lab/AdapterDateFns";
-// import LocalizationProvider from "@material-ui/lab/LocalizationProvider";
-import DateTimePicker from "@material-ui/lab/DateTimePicker";
 
-export default function BasicDateTimePicker() {
-  const [value, setValue] = React.useState(new Date());
-
+function Home() {
   return (
-    <LocalizationProvider dateAdapter={AdapterDateFns}>
-      <DateTimePicker
-        renderInput={(props) => <TextField {...props} />}
-        label="DateTimePicker"
-        value={value}
-        onChange={(newValue) => {
-          setValue(newValue);
-        }}
-      />
-    </LocalizationProvider>
-  );
+    <div>
+      
+    </div>
+  )
 }
+
+export default Home
+
+
 
 
 
