23eacc303e4676e97ff9957de9f747d7452053bf
chiragpatel500 <chirag12354@gmail.com>
2021-08-10 08:52:38 +0200

    clock update

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,61 @@
-
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "date-fns";
+import Grid from "@material-ui/core/Grid";
+import DateFnsUtils from "@date-io/date-fns";
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
+  KeyboardDatePicker,
+} from "@material-ui/pickers";
 
-function Home() {
+export default function Home() {
+  const [selectedDate, setSelectedDate] = React.useState(
+    new Date("2021-08-18T21:11:54")
+  );
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
+
   return (
-    <div>
-      
-    </div>
-  )
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <Grid container justifyContent="space-around">
+        <KeyboardDatePicker
+          disableToolbar
+          variant="inline"
+          format="MM/dd/yyyy"
+          margin="normal"
+          id="date-picker-inline"
+          label="Date picker inline"
+          value={selectedDate}
+          onChange={handleDateChange}
+          KeyboardButtonProps={{
+            "aria-label": "change date",
+          }}
+        />
+
+        <KeyboardTimePicker
+          margin="normal"
+          id="time-picker"
+          label="Time picker"
+          value={selectedDate}
+          onChange={handleDateChange}
+          KeyboardButtonProps={{
+            "aria-label": "change time",
+          }}
+        />
+        <KeyboardTimePicker
+          margin="normal"
+          id="time-picker"
+          label="Time picker"
+          value={selectedDate}
+          onChange={handleDateChange}
+          KeyboardButtonProps={{
+            "aria-label": "change time",
+          }}
+        />
+      </Grid>
+    </MuiPickersUtilsProvider>
+  );
 }
-
-export default Home
-
-
-
-
-
-
-
--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -27,7 +27,7 @@ export default function ButtonAppBar() {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" className={classes.title}>
+          <Typography variant="h6"  className={classes.title}>
             <Link to="/TimeLog">TimeLog</Link>
           </Typography>
           <Typography variant="h6" className={classes.title}>
